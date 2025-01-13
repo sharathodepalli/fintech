a5a7607a6f50fc65f06f6dbfd0cccf8879e18c15
sharathodepalli <sharathodepalli@gmail.com>
2025-01-12 23:28:04 -0500

    Updated code

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fintech/', 
+  base: '/fintech/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
